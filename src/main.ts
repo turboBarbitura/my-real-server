e096b72e5b5cc25ab5acf527a25d256f2bae4763
Mikhail Shchukin <turbobarbitura@mail.ru>
2025-03-12 09:30:34 +0700

    feat(deploy): поправил конфиги для vercel

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('API Documentation')
-    .setDescription('The API description')
-    .setVersion('1.0')
+    .setTitle('My Real Server API')
+    .setDescription('Докуменатци API сервиса для тренировки запросов с клиентской части')
+    .setVersion('0.1')
     .build();
   const document = SwaggerModule.createDocument(app, config);
 
